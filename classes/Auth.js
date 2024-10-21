2986d7452441381078c251cc547cac161447c19b
LiamSaul-Sky <liam.saul@sky.uk>
2024-10-21 16:46:41 +0100

    All changes made

---
+++ classes/Auth.js
@@ -0,0 +1,23 @@
+const {Customer} = require("./Customer.js");
+
+class Auth{
+    constructor(){
+        this.customers = [];
+    }
+    register(name, email, shippingAddress){
+        let newCustomer = new Customer(name, email, shippingAddress);
+        this.customers.push(newCustomer);
+    }
+    login(email){
+        for(let i =0; i<this.customers.length; i++){
+            if (this.customers[i].email === email){
+                return this.customers[i];
+            }
+            else{
+                return null;
+            }
+        }
+    }
+}
+
+module.exports = {Auth};
